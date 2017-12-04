8de02af491637b13f59b19eeb92f6eb95b37ae8b
Eric Mitchell <eric.mitchell@firstagain.local>
2017-12-04 09:35:45 -0800

    initial checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
   title = 'app';
+
+  ngOnInit() {
+    $(document).foundation();
+  }
 }
