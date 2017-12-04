d801d38b7d9887ab78aab92d7d076e40dd2d3c95
Eric Mitchell <eric@Erics-MacBook-Air.local>
2017-12-04 11:43:57 -0800

    Added tabs to example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   constructor() {}
