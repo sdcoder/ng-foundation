f94a78bc6a867fad1a7529a28838b11663e2b9a2
Eric Mitchell <eric@ericmitchell.net>
2017-12-18 14:12:19 -0800

    Added data bound text to tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   title = 'app';
+  panel1 = 'Tab #1 content';
+  // tslint:disable-line
+  panel2 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   ngOnInit() {
     $(document).foundation();
